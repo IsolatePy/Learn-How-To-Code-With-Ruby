beb3da396a4ba044ddecab28837a3d8d4360d9f3
IsolatePy <ali.arb74@gmail.com>
2019-09-15 15:15:08 +0430

    mc

--- Intro_to_Object_methods.rb
+++ Intro_to_Object_methods.rb
@@ -3,3 +3,20 @@
  p " hello world!".upcase
 
  p "HELLO WORLD".downcase
+
+foo = "Hello World!"
+
+p foo.length
+p foo.upcase
+
+
+puts
+
+p 10.next
+
+p -1.next
+
+puts "Hello World"
+puts "Hello World".inspect
+
+p "Hello World"
