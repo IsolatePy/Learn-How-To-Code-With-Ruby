b9294d3222b87a00e4883d6771a23118cd79df15
IsolatePy <ali.arb74@gmail.com>
2019-09-15 12:44:48 +0430

    MC

---
+++ Comments.rb
@@ -0,0 +1,6 @@
+# This is comment
+
+
+# This prints out "Hello world" to the vscode output
+
+puts "Hello World!"
---
+++ Variable.rb
@@ -0,0 +1,23 @@
+name = "Ali"
+last_name = "Arabi"
+handsome = true
+age = 23 + 1
+
+puts name
+puts last_name
+
+puts name + " " + last_name
+
+puts handsome
+
+puts age + 5
+
+age = 50
+
+puts age
+
+favorite_number = 24
+p favorite_number
+
+favorite_number = "Twenty-Four"
+p favorite_number
