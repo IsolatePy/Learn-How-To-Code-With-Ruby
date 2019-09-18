60c319711d9448bc44dcae66f2a4f8049f102fda
Ali <ali.arb74@gmail.com>
2019-09-18 10:31:48 +0430

    MC

---
+++ Strings I/Extract_Single_Characters_from_String_with_Bracket_Syntax.rb
@@ -0,0 +1,13 @@
+story = "Once upon a time in a land far , far away"
+
+p story.length
+
+p story[100]
+
+
+#nil object is something it doesnt exist
+#you can get index position with slice too
+p story.slice(3)
+
+p story[-4]
+p story.slice(-4)
