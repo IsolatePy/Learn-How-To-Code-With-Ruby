22115756776dc7baff2fe8c8a4f78d0777adf842
IsolatePy <ali.arb74@gmail.com>
2019-09-23 13:44:52 +0330

    MC

---
+++ Methods and Conditionals I/Default_or_Optional_Parameters.rb
@@ -0,0 +1,11 @@
+def make_phone_call (number,international_code,area_code)
+  if number.length == 8 && international_code == "98" && area_code == "21"
+    puts "Calling +#{international_code}#{area_code}#{number}"
+  else
+    puts "Only Iran Phone allowd"
+
+  end
+end
+
+
+make_phone_call("22123456","98","21")
--- Methods and Conditionals I/The_Ternary_Operator.rb
+++ Methods and Conditionals I/The_Ternary_Operator.rb
@@ -6,5 +6,47 @@ end
 
 
 #you can write if statement in one line with ? and then string then : then string
+#its exact same thing of above
 
 puts 1 < 4 ? "Yes, It is" : "No, It's not!"
+
+
+if "yes" == "yes"
+  puts "The two are equal"
+else
+  puts "The two are not equal"
+end
+
+puts "no" == "yes" ? "The two are equal" : "The two are not equal"
+
+
+def even_or_odd(number)
+
+number.even? ? "The number is even" : "the number is odd"
+
+
+end
+
+p even_or_odd(5)
+
+
+
+pokemon = "Pikachu"
+
+if pokemon == "Charizard"
+  puts "Fireball!"
+else
+  puts "That is not Charizard!"
+end
+
+
+
+
+def check_pokemon(pokemon)
+  pokemon == "Charizad" ? "Fireball!" : "That is not Charizard!"
+end
+
+p check_pokemon("Pikachue")
+p check_pokemon("Squirtle")
+p check_pokemon("Onix")
+p check_pokemon("Charizad")
