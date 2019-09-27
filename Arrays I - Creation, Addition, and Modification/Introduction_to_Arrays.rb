fa8d714cbe037d0d92dad2006397fdb433b10c46
Ali <ali.arb74@gmail.com>
2019-09-27 13:03:31 +0330

    MC

--- Arrays I - Creation, Addition, and Modification/Introduction_to_Arrays.rb
+++ Arrays I - Creation, Addition, and Modification/Introduction_to_Arrays.rb
@@ -2,4 +2,30 @@ p []
 p [].class
 
 
-numbers = []
+numbers = [4,5,6,7,8,9]
+p numbers
+
+toys = ["Teddy bear" , "super soaker" , "board game"]
+p toys
+
+x = 1..100
+ p x.to_a.shuffle
+
+ #with << we can add sth in array
+
+a = [1,2,3,4,5,6,7,8,9]
+a << 10
+# with unshift you can add string to the first of array
+#with append you can add string to the last of array
+a.unshift("Ali")
+a.append("Ali")
+a.uniq!
+#with uniq you can remove duplicate from array
+#if you want to persist it you can use !
+p a.empty?
+#with push method you can add item to array
+a.push("Sara")
+
+b = a.pop
+p b
+p a
---
+++ Arrays I - Creation, Addition, and Modification/More_With_Array.rb
@@ -0,0 +1,10 @@
+a = [1,2,3,4,5,6,7,8,9,"Ali"]
+p a.join
+b = a.join("-")
+p b.split("-")
+
+
+#you can write array with %w
+z = %w(my name is Ali and this is great Ruby is amazing)
+p z
+p z[0]
