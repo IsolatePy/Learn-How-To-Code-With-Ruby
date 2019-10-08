a2845aa6d0e571a028d461909fc5a213de320bd9
IsolatePy <ali.arb74@gmail.com>
2019-10-08 16:15:43 +0330

    MC

--- 16- Classes I/gadget.rb
+++ 16- Classes I/gadget.rb
@@ -1,31 +1,34 @@
 # you need to capitalize the first letter of class
 class Gadget
-  def initialize
-    @username = "User #{rand(1..100)}"
-    @password = "topsecret"
+  attr_accessor :username # It make the getter and setter and have less code
+  # attr_accessor is better
+  attr_reader :production_number
+  attr_writer :password
+  # we have to just let to password being change not expose
+  # you can set password but you cant read it and expose it
+  def initialize(username,password)
+    @username = username
+    @password = password
     @production_number = "#{("a".."z").to_a.sample}-#{rand(1..99)}"
-    @self = "#{rand(200..300)}"
     end
   def info
-  "Gadget #{@production_number} has the username #{@username}. IT is made from the #{@self} class and it has the ID #{@self}"
+  "Gadget #{@production_number} has the username #{@username}."
  end
 # This is the getter method , only gets for us and expose it
-  def username
-    @username
-
- end
-# Getter Method
-  def password
-    @password
- end
-
-  def production_number
-    @production_number
- end
-
+# setter will let you to change attribute
 end
 
-phone = Gadget.new
-p phone.username
-p phone.password
-p phone.production_number
+g1 = Gadget.new("rubyfan102", "programming123")
+p g1.username
+p g1.production_number
+g1.password = "blalalahalalb"
+g1.username = "anothergreatname"
+p g1.username
+
+g2 = Gadget.new("misterprogrammer", "bestpassever")
+g2.username = "Mrs. Programmer"
+p g2.username
+
+g3 = Gadget.new("sportsfan100", "topsecret")
+g2.password = "besttopsecret"
+p g3.production_number
